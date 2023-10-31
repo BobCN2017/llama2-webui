3d7b7aa019704747ed062588f97d57cba8a142a6
zhangsongbo <zhangp365@gmail.com>
2023-10-31 17:20:33 +0800

    added fetching prompts from remote.

--- prompts/utils.py
+++ prompts/utils.py
@@ -1,12 +1,17 @@
 import csv
+import json
 import os
 from hashlib import md5
+import requests
+import logging
+
 
 def read_csv_to_dict_list(file_path):
     with open(file_path, mode='r', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         list_of_dicts = [row for row in reader]
         return list_of_dicts
+
 
 def split_list_with_key(lst, dict_key):
     result = {}
@@ -16,24 +21,58 @@ def split_list_with_key(lst, dict_key):
         result[row.get(dict_key)].append(row)
     return result
 
+
 def read_csv_to_type_dict(file_path, type_key):
     lst = read_csv_to_dict_list(file_path=file_path)
-    return split_list_with_key(lst=lst, dict_key= type_key)
+    return split_list_with_key(lst=lst, dict_key=type_key)
+
 
 def md5_str(str):
     return md5(str.encode("utf8")).hexdigest()
 
+
+def fetch_list_from_remote(language):
+    param = {
+        "appcategory": "LLM",  # LLM、SD、SDXL
+        "language": language     # zh: 中文   en: 英文
+    }
+
+    try:
+        url = "https://api.tensorbee.com/api/prompt/getenabledlist"
+        result = requests.post(url, json=param).text
+        result = json.loads(result)
+        if result.get("code") != 0:
+            return []
+        else:
+            return result.get("data")
+    except Exception as e:
+        logging.exception(e)
+        return []
+
+
 current_dir = os.path.dirname(__file__)
+
 
 class PromtsContainer(object):
 
     def __init__(self) -> None:
-        prompts_path = os.path.join(current_dir,"prompts_zh.csv")
-        self.data = read_csv_to_type_dict(prompts_path,"type")
-        self.summary_dict = {md5_str(row.get("summary")):row.get("prompt")  for chunk in self.data.values() for row in chunk }
+        remote_list = fetch_list_from_remote("zh")
+        if len(remote_list) > 0:
+            logging.info("fetch prompts from remote.")
+            self.data = split_list_with_key(remote_list, "type")
+        else:
+            logging.info("get prompts from local.")
+            prompts_path = os.path.join(current_dir, "prompts_zh.csv")
+            self.data = read_csv_to_type_dict(prompts_path, "type")
+        self.summary_dict = {md5_str(row.get("summary")): row.get(
+            "prompt") for chunk in self.data.values() for row in chunk}
 
     def get_prompts_tab_dict(self):
         return self.data
-    
+
     def get_prompt_by_summary(self, summary):
-        return self.summary_dict.get(md5_str(summary),summary)
+        return self.summary_dict.get(md5_str(summary), summary)
+
+
+if __name__ == "__main__":
+    print(fetch_list_from_remote("en"))
