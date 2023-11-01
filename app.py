694ab03e6866109661806d59e76d93f967a76b42
zhangsongbo <zhangp365@gmail.com>
2023-11-01 10:47:54 +0800

    added a text-left-aligned css.

--- app.py
+++ app.py
@@ -158,7 +158,8 @@ def main():
                         prompt_text = gr.Button(
                             label="",
                             value=f"{item['summary']}",
-                            size="sm"
+                            size="sm",
+                            elem_classes='text-left-aligned'
                         )
                         prompt_text.click(
                             fn=save_textbox_for_prompt,
@@ -200,6 +201,7 @@ def main():
         .contain { display: flex; flex-direction: column;}
         #component-0 #component-1 #component-2 #component-4 #component-5 { height:71vh !important; }
         #component-0 #component-1 #component-24 > div:nth-child(2) { height:80vh !important; overflow-y:auto }
+        .text-left-aligned {text-align: left !important; font-size: 16px;}
     """
     with gr.Blocks(css=CSS) as demo:
         with gr.Row(equal_height=True):
